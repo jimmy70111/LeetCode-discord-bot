5686984e5c5dd4456cba8832e660dd0df3e1ef31
jimmy <jimmyjiang70@gmail.com>
2024-06-12 15:47:15 -0400

    made conf to hide on git

--- sqlconnect.java
+++ sqlconnect.java
@@ -1,27 +1,35 @@
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.Properties;
 
 public class sqlconnect {
+    public static Connection getConnection() throws SQLException, IOException {
+        Properties props = new Properties();
+        try (InputStream in = new FileInputStream("config.properties")) {
+            props.load(in);
+        }
 
-    private static final String URL = "jdbc:mysql://localhost:3306/leetcode_leaderboard";
-    private static final String USER = "root";
-    private static final String PASSWORD = "jimmyjiang2264160985";
+        String url = props.getProperty("db.url");
+        String user = props.getProperty("db.user");
+        String password = props.getProperty("db.password");
 
-    public static Connection getConnection() throws SQLException {
-        return DriverManager.getConnection(URL, USER, PASSWORD);
+        return DriverManager.getConnection(url, user, password);
     }
 
     public static void main(String[] args) {
         try {
-            Connection connection = sqlconnect.getConnection(); 
+            Connection connection = sqlconnect.getConnection();
             if (connection != null) {
                 System.out.println("Connection successful!");
                 connection.close(); // Close the connection when done
             } else {
                 System.out.println("Failed to connect to the database.");
             }
-        } catch (SQLException e) {
+        } catch (SQLException | IOException e) {
             System.err.println("Error connecting to the database: " + e.getMessage());
         }
     }
